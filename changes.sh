a087f1865d62267e3ee889d33e83313f8b2e4904
cliserkad <bryanchristopherjohnson@gmail.com>
2021-01-30 16:00:10 -0500

    Simplify changes.sh

--- changes.sh
+++ changes.sh
@@ -5,9 +5,6 @@ rm change.log
 
 printf "Since last tag:\n" >> change.log
 printf "$(git log --pretty=format:"%s" $(git describe --tags --abbrev=0 @^)..@ --no-merges | sed -e :1 -e 's/\([.?!]\)[[:blank:]]\{1,\}\([^[:blank:]]\)/\1\n\2/;t1')" | sed -e 's/^/\t/' >> change.log
-printf "\n\n" >> change.log
-printf "Last 10 changes:\n" >> change.log
-printf "$(git log --pretty=format:"%s" -10 --no-merges | sed -e :1 -e 's/\([.?!]\)[[:blank:]]\{1,\}\([^[:blank:]]\)/\1\n\2/;t1')" | sed -e 's/^/\t/' >> change.log
 export CHANGES=$(cat change.log)
 
 printenv CHANGES
