c2ad835b9cb829ccbb39c79d33ce6ae4b29019a2
Bryan Christopher Johnson <bryanchristopherjohnson@gmail.com>
2021-02-08 12:46:25 -0500

    Use curl instead of powershell. Add launch script to release.yml

--- src/main/resources/windows/installer.cmd
+++ src/main/resources/windows/installer.cmd
@@ -1,5 +1,5 @@
 :: init
-TITLE Bitter Installer (.bat for Windows)
+TITLE Bitter Installer (.cmd for Windows)
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 set install-dir=%HOMEPATH%\bitter
 set jar-loc=%install-dir%\bitter.jar
@@ -8,7 +8,8 @@ set repo-loc=%install-dir%\bitter
 :: download bitter.jar
 md %install-dir%
 cd %install-dir%
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/cliserkad/bitter/releases/latest/download/bitter.jar', '%jar-loc%')"
+curl -L https://github.com/cliserkad/bitter/releases/latest/download/bitter.jar --output %jar-loc%
+curl -L https://github.com/cliserkad/bitter/releases/latest/download/bitter.cmd --output %install-dir%\bitter.cmd
 
 :: clone git repo
 del %repo-loc% /F /Q
@@ -22,4 +23,8 @@ cd %install-dir%
 mklink "%HOMEPATH%\Desktop\Bitter" %jar-loc%
 
 :: run
-powershell -Command "javaw -jar %jar-loc%"
+start javaw -jar %jar-loc%
+
+:: display output
+echo "You may now close this window, or inspect the output."
+pause
